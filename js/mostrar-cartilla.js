ae976cf3556c1d82b5e37fe4db02fb9b059d484c
Luis Angel Riaño Calixto <67494191+LuisAngelRianoC@users.noreply.github.com>
2024-03-07 02:12:20 -0600

    cuarta actualizacion

--- js/mostrar-cartilla.js
+++
@@ -1,5 +0,0 @@
-function openPopup() {
-    // Llama a la función mostrarInfo definida en el popup.html
-    // Cambia el 1 por el id que deseas mostrar
-    document.getElementById('info1').style.display = 'block';
-}
